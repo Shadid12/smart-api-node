40aaaf2e14e01c4944dd13f3dfc76441d0e651be
shadid2018 <shadidhaque2012@gmail.com>
2018-10-26 19:49:09 -0400

    added new shit

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+const patientsRoutes = require('./api/routes/patients');
+app.use('/patients', patientsRoutes);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
 
 app.use((req, res, next) => {
     res.status(200).json({
